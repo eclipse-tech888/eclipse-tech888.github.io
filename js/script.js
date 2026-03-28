bab0f38b426f94b40ff6c5c6a8aadb6052e1a790
ベーやん <b82369203@gmail.com>
2026-03-28 20:53:03 +0900

    Implement omikuji button functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-/* 準備中 */
+// 全体部分
+
+// おみくじ部分
+const omikujiButton = document.getElementById("omikuji-button");
+const omikujiResult = document.getElementById("omikuji-result");
+
+omikujiButton.addEventListener("click", () => {
+    const rand = Math.floor(Math.random() * 3)
+    
+    if (rand === 0) {
+        omikujiResult.textContent = "大吉";
+    } else if (rand === 1) {
+        omikujiResult.textContent = "中吉";
+    } else {
+        omikujiResult.textContent = "小吉";
+    }
+});
